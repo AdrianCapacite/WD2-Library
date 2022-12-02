64709492820601b675bf2b19d8883c1a7b9143b6
Adrian Capacite <capacite.adrian.thomas@gmail.com>
2022-12-02 01:49:11 +0000

    added nav partial

---
+++ includes/partials/nav.php
@@ -0,0 +1,15 @@
+<nav>
+    <a href="./">Home</a>
+    <!-- Book Search -->
+    <form action="books.php" method="get">
+        <input type="text" name="search" id="search" placeholder="Search">
+        <input type="submit" value="Search">
+    </form>
+    <!-- Dropdown -->
+    <a href="#">User</a>
+    <ul>
+        <li><a href="./reserved.php">Reserved Books</a></li>
+        <li><a href="./membership.php">Membership</a></li>
+        <li><a href="./logout.php">Logout</a></li>
+    </ul>
+</nav>
