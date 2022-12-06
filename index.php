fa41b1592afff4ce12138d5238ca6d32dfd0e0d2
Adrian Capacite <capacite.adrian.thomas@gmail.com>
2022-12-06 13:40:21 +0000

    index.php

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
+
 <?php
-  $pageTitle = "Home";
-  require_once './includes/partials/header.php';
+$pageTitle = "Home";
+$navVisible = true;
+require_once './includes/loader.php';
+require_once './includes/partials/header.php';
+
+if (!isLoggedIn()) {
+  header("Location: ./login.php");
+}
+
+require_once './includes/partials/header.php';
 ?>
 
 <?php
-  require_once './includes/partials/footer.php';
+require_once './includes/partials/footer.php';
 ?>
