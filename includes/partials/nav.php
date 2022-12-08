076a2b6250f8a602bf71c91b08e51f7edbe22654
Adrian Capacite <capacite.adrian.thomas@gmail.com>
2022-12-08 01:43:12 +0000

    Refactored book search

--- includes/partials/nav.php
+++ includes/partials/nav.php
@@ -1,37 +1,14 @@
 <nav>
     <a href="./">Home</a>
-    <!-- Book Search -->
-    <form action="books.php" method="get">
-        <input type="text" name="search" id="search" placeholder="Search title or author">
-        <select name="category" id="category">
-            <option value="">All</option>
-            <?php
-            foreach (getCategories() as $category) {
-                echo "<option value='$category'>$category</option>";
-            }
-            ?>
 
-        </select>
-        <input type="submit" value="Search">
-    </form>
+    <?php include './includes/partials/book-search.php' ?>
 
-    <!-- Dropdown -->
-    <!-- <button id="user-dropdown" class="drop-down" onclick="toggleActive(this);">
-        <span>User</span>
-        <div class="drop-down__content">
-            <ul>
-                <li><a href="./reserved.php">Reserved Books</a></li>
-                <li><a href="./membership.php">Membership</a></li>
-                <li><a href="./logout.php">Logout</a></li>
-            </ul>
-        </div>
-    </button> -->
     <div class="dropdown">
         <input hidden type="checkbox" id="user-dropdown-toggle" class="dropdown__toggle">
         <label for="user-dropdown-toggle" class="dropdown__label">User</label>
         <div class="dropdown__content">
             <ul>
-                <li><a href="./reserved.php">Reserved Books</a></li>
+                <li><a href="./reserved-books.php">Reserved Books</a></li>
                 <li><a href="./membership.php">Membership</a></li>
                 <li><a href="./logout.php">Logout</a></li>
             </ul>
